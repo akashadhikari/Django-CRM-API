b73b552230a142caed743f732de8317043fcdf96
Akash Adhikari <akash_532998@yahoo.com>
2018-01-12 12:49:22 +0545

    communication 2 models test

--- communication/tests/test_models.py
+++ communication/tests/test_models.py
@@ -27,22 +27,23 @@ class ClientlistTest(TestCase):
         new_client = Clientlist.objects.get(client_name="Apple")
         self.assertEqual(new_client.contact_person, "Mr. Foo Bar")
 
-# class SalesStageTest(TestCase):
+class SalesStageTest(TestCase):
 
-#     def setUp(self):
-#         user = User.objects.create(username="nerd")
-#         client = Clientlist.objects.create(client_name="Apple", user = user)
-#         SalesStage.objects.create(
-#             substage = 'Contact verification', sales_stage= 'Suspecting', client = client
-#             )
-#         SalesStage.objects.create(
-#             substage = 'Client detail', sales_stage='Prospecting', client = client
-#             )
+    def setUp(self):
+        user = User.objects.create(username="nerd")
+        lead = LeadProcess.objects.create(service="Hardware", user=user)
+        client = Clientlist.objects.create(client_name="Apple", user = user, lead = lead)
+        SalesStage.objects.create(
+            substage = 'Contact verification', sales_stage= 'Suspecting', client = client
+            )
+        SalesStage.objects.create(
+            substage = 'Client detail', sales_stage='Prospecting', client = client
+            )
 
-#     def test_salesstage_status(self):
-#         salesstage_suspecting = SalesStage.objects.get(sales_stage='Suspecting')
-#         salesstage_prospecting = SalesStage.objects.get(sales_stage = 'Prospecting')
-#         self.assertEqual(
-#             salesstage_suspecting.get_substage(), "Contact verification falls under Suspecting")
-#         self.assertEqual(
-#             salesstage_prospecting.get_substage(), "Client detail falls under Prospecting")
+    def test_salesstage_status(self):
+        salesstage_suspecting = SalesStage.objects.get(sales_stage='Suspecting')
+        salesstage_prospecting = SalesStage.objects.get(sales_stage = 'Prospecting')
+        self.assertEqual(
+            salesstage_suspecting.get_substage(), "Contact verification falls under Suspecting")
+        self.assertEqual(
+            salesstage_prospecting.get_substage(), "Client detail falls under Prospecting")
