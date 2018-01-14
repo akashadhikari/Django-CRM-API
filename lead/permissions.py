93061f2e3cd803a8109b7e140d60305f66cceb16
Akash Adhikari <akash_532998@yahoo.com>
2018-01-14 15:03:21 +0545

    permission message chaneg

--- lead/permissions.py
+++ lead/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 class IsOwnerOrReadOnly(BasePermission):
-	message = 'So, you wanna edit, huh? Why don\'t you just go to hell?'
+	message = 'This is not your client. So, mind your own business, buddy!'
 	def has_object_permission(self,request, view, obj):
 		return obj.user == request.user
