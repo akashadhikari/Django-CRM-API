456d99d2030b6eaad40d27e56c8bf8004c5bc1e2
Akash Adhikari <akash_532998@yahoo.com>
2018-01-21 15:17:53 +0545

    Nested Create Read Delete

---
+++ common/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from .models import Album, Track
+
+class TrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Track
+        fields = ('id', 'user', 'album', 'order', 'title', 'duration')
+
+class AlbumSerializer(serializers.ModelSerializer):
+    tracks = TrackSerializer(many=True)
+
+    class Meta:
+        model = Album
+        fields = ('user', 'album_name', 'artist', 'tracks')
+
+    def create(self, validated_data):
+        tracks_data = validated_data.pop('tracks')
+        album = Album.objects.create(**validated_data)
+        for track_data in tracks_data:
+            Track.objects.create(album=album, **track_data)
+        return album
--- common/views.py
+++ common/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+import django_filters.rest_framework
+from rest_framework import generics
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
+	)
 
-# Create your views here.
+from rest_framework import filters
+
+from .permissions import IsOwnerOrReadOnly
+from .models import (
+	Album,
+	Track,
+	)
+from .serializers import (
+	AlbumSerializer,
+	TrackSerializer,
+	)
+
+class AlbumViewSet(generics.ListCreateAPIView):
+	queryset = Album.objects.all()
+	serializer_class = AlbumSerializer
+	#permission_classes = (IsAuthenticated,)
+
+	filter_backends = (
+		filters.SearchFilter,
+		filters.OrderingFilter,
+		django_filters.rest_framework.DjangoFilterBackend,
+		)
+	# filter_fields = ('album_incharge', 'Album_address')
+	# search_fields = ('album_incharge', 'Album_address')
+
+
+	def perform_create(self, serializer):
+			serializer.save() # Adding owner=self.request.user
+
+class AlbumDetailsViewSet(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Album.objects.all()
+	serializer_class = AlbumSerializer
+	permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+
+class TrackViewSet(generics.ListCreateAPIView):
+	queryset = Track.objects.all()
+	serializer_class = TrackSerializer
+	#permission_classes = (IsAuthenticated,)
+
+	filter_backends = (
+		filters.SearchFilter,
+		filters.OrderingFilter,
+		django_filters.rest_framework.DjangoFilterBackend,
+		)
+	# filter_fields = ('outflowed_to', 'service_outflowed')
+	# search_fields = ('outflowed_to', 'service_outflowed')
+
+
+	def perform_create(self, serializer):
+			serializer.save() # Adding owner=self.request.user
+
+class TrackDetailsViewSet(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Track.objects.all()
+	serializer_class = TrackSerializer
+	permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
