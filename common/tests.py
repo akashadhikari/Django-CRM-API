a95f18fde3d083e9defbe079e5af0a90bffa666a
Akash Adhikari <akash_532998@yahoo.com>
2018-02-01 08:13:32 +0545

    user creation test

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from django.contrib.auth.models import User
+
+class UserTests(APITestCase):
+
+    def test_create_user(self):
+        """
+        Ensure we can create a new user object.
+        """
+        url = reverse('get_post_user')
+        data = {
+            "username": "john",
+            "email": "john@snow.com",
+            "password": "you_know_nothing"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
